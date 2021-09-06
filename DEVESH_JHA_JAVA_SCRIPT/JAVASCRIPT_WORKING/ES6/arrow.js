0fd069e19f054407570765c49a1ba5fd05e00be5
devesh <48615331+djpredator@users.noreply.github.com>
2021-09-06 11:06:36 +0530

    Add files via upload

---
+++ DEVESH_JHA_JAVA_SCRIPT/JAVASCRIPT_WORKING/ES6/arrow.js
@@ -0,0 +1,10 @@
+console.clear();
+
+function greetings(name) {
+    return `welcome ${name}`
+}
+const mGreetings = name => `welcome ${name}`
+
+//const mGreetings = (name, age
+console.log(greetings('John'));
+console.log(mGreetings('John'));
